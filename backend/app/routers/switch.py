5422e97781c55f029421209e9917b9b51258f62f
Enzo Mendez <enzo.mendez315@gmail.com>
2025-04-02 16:42:39 -0600

    Add ports endpoints.

--- backend/app/routers/switch.py
+++ backend/app/routers/switch.py
@@ -1,3 +1,4 @@
+from urllib.parse import unquote
 from dotenv import load_dotenv
 import os
 
@@ -22,3 +23,17 @@ async def get_switch_vlans(switch_ip: str):
 @router.get("/{switch_ip}/vlans/{vlan_id}")
 async def get_switch_vlan_ports(switch_ip: str, vlan_id: int):
     return SwitchService.get_vlan_ports(switch_ip, username, password, vlan_id)
+
+@router.get("/{switch_ip}/ports")
+async def get_switch_ports(switch_ip: str):
+    return SwitchService.get_all_ports_status(switch_ip, username, password)
+
+@router.get("/{switch_ip}/ports/{port_id}")
+async def get_switch_port_config(switch_ip: str, port_id: str):
+    port_id = port_id.replace("-", "/")
+    return SwitchService.get_port_config(switch_ip, username, password, port_id)
+
+@router.get("/{switch_ip}/ports/{port_id}/status")
+async def get_switch_port_status(switch_ip: str, port_id: str):
+    port_id = port_id.replace("-", "/")
+    return SwitchService.get_port_status(switch_ip, username, password, port_id)
--- backend/app/services/switch_service.py
+++ backend/app/services/switch_service.py
@@ -27,3 +27,42 @@ class SwitchService:
                 return f"Authentication failed to host {host}"
         except Exception as e:
             print(f"Switch service failed to get the ports on VLAN {vlan_id} on {host}: {e}")
+
+    @staticmethod
+    def get_all_ports_status(host: str, username: str, password: str):
+        try:
+            net_connect = ssh_connect(host, username, password)
+            if net_connect:
+                output = net_connect.send_command(f"show int status")
+                net_connect.disconnect()
+                return output
+            else:
+                return f"Authentication failed to host {host}"
+        except Exception as e:
+            print(f"Switch service failed to get the ports' status on {host}: {e}")
+
+    @staticmethod
+    def get_port_config(host: str, username: str, password: str, port: str):
+        try:
+            net_connect = ssh_connect(host, username, password)
+            if net_connect:
+                output = net_connect.send_command(f"show run int {port}")
+                net_connect.disconnect()
+                return output
+            else:
+                return f"Authentication failed to host {host}"
+        except Exception as e:
+            print(f"Switch service failed to get the port's ({port}) configuration on {host}: {e}")
+
+    @staticmethod
+    def get_port_status(host: str, username: str, password: str, port: str):
+        try:
+            net_connect = ssh_connect(host, username, password)
+            if net_connect:
+                output = net_connect.send_command(f"show int status | i {port}")
+                net_connect.disconnect()
+                return output
+            else:
+                return f"Authentication failed to host {host}"
+        except Exception as e:
+            print(f"Switch service failed to get the port's ({port}) configuration on {host}: {e}")
